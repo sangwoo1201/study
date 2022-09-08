57ec6a3323186eee7a2bea095cb03892c274d647
sangvely <111099652+sangwoo1201@users.noreply.github.com>
2022-09-08 12:16:56 +0900

    0908 수업내용 업로드

---
+++ spring-workspaces/src/study/main/java/com/example/study/vo/DeptVO.java
@@ -0,0 +1,26 @@
+package com.example.study.vo;
+
+public class DeptVO {
+
+	private int deptno; //부서번호
+	private String dname; //부서이름
+	private String loc; //근무지
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+}
